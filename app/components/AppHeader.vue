fb484f6e912b5fb1a2b62c0b86e86af6ac47dd8c
Andrea Contesini <andrea.contesini@beconcept.studio>
2026-04-09 09:21:15 +0200

    Fix header nav

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,9 +3,7 @@
         <div class="div--container relative py-5">
             <div class="grid grid-cols-12 gap-5">
                 <div class="col-span-4 self-center flex items-center gap-16">
-                    <a id="logo-d" class="block w-full max-w-[180px]" href="#">
-                        Logo
-                    </a>
+                    <NuxtLink to="/" class="block w-full max-w-[180px]">Logo</NuxtLink>
                 </div>
                 <div class="col-span-8 self-center justify-end flex items-center h-full gap-4">
                     <AppNav />
