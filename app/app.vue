f6c08e1ceca3cd3c0b55fefcecd0f8296f4c09ac
Andrea Contesini <andrea.contesini@beconcept.studio>
2026-04-13 15:29:34 +0200

    Fix loader effect in pages

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,13 @@
+<script setup lang="ts">
+  const { isFirstLoad } = useAppState()
+  const route = useRoute()
+  const isHome = computed(() => route.path === '/')
+</script>
 <template>
-  
-  <AppLoaderHome/>
-
+  <AppLoaderHome v-if="isHome , isFirstLoad" />
+  <AppLoaderDefault v-else="!isHome , isFirstLoad" />
   <NuxtLayout>
     <NuxtPage :transition="isFirstLoad ? false : undefined" />
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-const { isFirstLoad } = useAppState()
-</script>
--- app/composables/useIntroAnimation.ts
+++ app/composables/useIntroAnimation.ts
@@ -112,15 +112,81 @@ export const useIntroAnimation = () => {
       const el = overlayRef.value
       if (!el) return resolve()
 
-      gsap.timeline({ onComplete: resolve })
-        .to(el.querySelector('.intro-curtain'), {
-          scaleY: 0,
-          transformOrigin: 'top',
-          duration: 0.8,
+      const logoItem = document.querySelector<HTMLElement>('#main-logo')
+      const navItem = document.querySelector<HTMLElement>('#header-desktop')
+      const titleEl = el.querySelector<HTMLElement>('.intro-slogan')
+      const textEl = el.querySelector<HTMLElement>('.intro-text')
+      const maskEl = el.querySelector<HTMLElement>('.intro-mask')
+      
+      if (!titleEl) return resolve()
+      if (!textEl) return resolve()
+      if (!maskEl) return resolve()
+      
+
+      const blurFrom = 6
+      const blurStep = 3
+
+      const tl = gsap.timeline({ onComplete: resolve })
+
+      tl.set(logoItem, {
+        y: -100,
+        autoAlpha: 0,
+      })
+      tl.set(navItem, {
+        y: 100,
+        autoAlpha: 0,
+      })
+      
+      tl.set(titleEl, {
+        filter: `blur(${blurFrom}px)`,
+      })
+      tl.set(maskEl, {
+        clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)',
+      })
+      tl.to(titleEl, {
+        autoAlpha: 1,
+        duration: 1,
+        ease: 'power3.out',
+        delay: 1,
+      })
+
+      for (let i = 1; i <= blurStep; i++) {
+        const percent = (i / blurStep) * 100
+        const newBlur = blurFrom - (i / blurStep) * blurFrom // scala da 6 → 0
+        tl.to(maskEl, {
+          clipPath: `polygon(0 0, ${percent}% 0, ${percent}% 100%, 0 100%)`,
+          duration: .65,
           ease: 'power4.inOut',
-        })
-        .set(el, { autoAlpha: 0 })
-        console.log('AnimazioneDefault')
+        }, '>')
+        tl.to(titleEl, {
+          filter: `blur(${newBlur}px)`, // ← backtick
+          duration: .5,
+          ease: 'power4.inOut',
+        } , '<')
+      }
+      
+      tl.to(logoItem, {
+        y: 0,
+        autoAlpha: 1,
+        duration: 1.5,
+        ease: 'power4.inOut',
+      }, '>+=1')
+      tl.to(navItem, {
+        y: 0,
+        autoAlpha: 1,
+        duration: 1.5,
+        ease: 'power4.inOut',
+      } , '<')
+
+      .to(el, {
+        autoAlpha: 0,
+        filter: 'blur(8px)',
+        duration: 1,
+        ease: 'power2.in',
+        onComplete: function () {
+          el.remove();
+        }  
+      }, '<')
     })
   }
 
