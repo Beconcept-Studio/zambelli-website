44c3f8ecc864abae12d031b53dd628f44ec0df4a
Andrea Contesini <andrea.contesini@beconcept.studio>
2026-04-14 13:57:54 +0200

    Block

---
+++ app/composables/useWorkTransitionState.ts
@@ -0,0 +1,9 @@
+// export const useWorkTransition = () => {
+//   const clickedRect = useState<DOMRect | null>('work-clicked-rect', () => null)
+//   const isTransitioning = useState('work-is-transitioning', () => false)
+
+//   return { 
+//     clickedRect, 
+//     isTransitioning 
+//   }
+// }
